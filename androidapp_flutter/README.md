d70ee1fe3dc3497213d20b02f235191e54fed3c6
s464837 <jaknie10@st.amu.edu.pl>
2025-05-22 08:36:03 +0200

    first commit

---
+++ androidapp_flutter/README.md
@@ -0,0 +1,3 @@
+# androidapp_flutter
+
+A new Flutter project.
---
+++ androidapp_flutter/android/app/src/main/kotlin/com/example/androidapp_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.androidapp_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
