dd0cbb0d1205fadd9f4e25d8252bf39b881a8beb
s464837 <jaknie10@st.amu.edu.pl>
2025-05-29 09:51:59 +0200

    aktualnosci poprawka 2

--- androidapp_flutter/lib/news.dart
+++ androidapp_flutter/lib/news.dart
@@ -37,10 +37,10 @@ Future<List<NewsItem>> fetchNews() async {
       await http.get(Uri.parse('https://wanatowka.pl/kiosk/api_client.php/'));
 
   if (response.statusCode == 200) {
-    final List<dynamic> data = json.decode(response.body);
-    return data.map((json) => NewsItem.fromJson(json)).toList();
+    final List<dynamic> jsonData = json.decode(response.body);
+    return jsonData.map((item) => NewsItem.fromJson(item)).toList();
   } else {
-    throw Exception('Failed to load news');
+    throw Exception('Failed to load data');
   }
 }
 
